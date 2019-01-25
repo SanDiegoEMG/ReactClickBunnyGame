c3bfa6d8125bff9c33e8a1331af17adcf29566b6
SD_emg <sandiegoemg@gmail.com>
2019-01-25 08:37:39 -0800

    Updates

---
+++ precache-manifest.f6dd6b3179cf7fa993dbcae91e41f5e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react2hw/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "02ff283f0ef567d225438e0e76cb88f1",
+    "url": "/react2hw/static/media/background-b-and-w.02ff283f.jpg"
+  },
+  {
+    "revision": "d8e66f1bce85958e5d7c",
+    "url": "/react2hw/static/js/runtime~main.d8e66f1b.js"
+  },
+  {
+    "revision": "411269702a23c3c1c827",
+    "url": "/react2hw/static/js/main.41126970.chunk.js"
+  },
+  {
+    "revision": "425193f9241e750fb12a",
+    "url": "/react2hw/static/js/1.425193f9.chunk.js"
+  },
+  {
+    "revision": "411269702a23c3c1c827",
+    "url": "/react2hw/static/css/main.e5111705.chunk.css"
+  },
+  {
+    "revision": "44dd2d9ff1d3e584d73741a94c69094e",
+    "url": "/react2hw/index.html"
+  }
+];
