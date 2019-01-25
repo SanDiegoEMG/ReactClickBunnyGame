74ab27807c94b27f1fce3edc974e6d456ddac677
SD_emg <sandiegoemg@gmail.com>
2019-01-25 08:30:16 -0800

    removed build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PictureCard from "./components/PictureCard";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
 class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    clickedArray: [],
+    topScore: 0,
+    score: 0,
+    message: "",
+    shakeit: "false"
+  };
+
+  clickPicture = id => {
+    // Arrange the pictures in a random manner
+    const shuffledArray = this.shuffleArray(cards);
+    this.setState({cards: shuffledArray});
+
+    // if clicked image already clicked set this.state.score = 0; empty clickeadArray, end of if block
+    if (this.state.clickedArray.includes(id)) {
+      this.setState({ score: 0, clickedArray: [], message: "Incorrect!! Game Over ☹️ Click an image to start again!", shakeit: "true"});
+    }
+    else {
+      this.setState({
+        clickedArray: this.state.clickedArray.concat([id]),
+        score: this.state.score + 1,
+        message: "Correct!! 🙂",
+        shakeit: "false"
+      });
+          // if user reaches max score of 12
+          // if (this.state.score === 12) {
+          // this.setState({ score: 0, clickedArray: [], message: "Congratulations, you are a WINNER!", shakeit:"false"});
+          //  }
+       }
+  
+    // set topscore = score if score>topscore.
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+
+  }
+
+  shuffleArray = (picturesArray) => {
+      for (let i = picturesArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+      }
+      return picturesArray;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <h1 className="App-title">Em's React Clicky Game</h1>
+
+          <p><strong>First click on each image will earn a point </strong> </p>
+
+            <p>Click on the same image more than once and the game will end</p>
+
+            <h3 className="App-intro">
+              <p className="message"><strong>{this.state.message}</strong></p>
+              <p className = "score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+            </h3>
         </header>
+
+        
+
+        <Wrapper
+        shakeWrapper = {this.state.shakeit}
+        pictures=
+          {this.state.cards.map(picture => (
+            <PictureCard
+              clickPicture={this.clickPicture}
+              id={picture.id}
+              key={picture.id} // to get rid of unique key prop warning
+              name={picture.name}
+              image={picture.image}
+            />
+          ))}
+        />
+
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">&copy;EmG - Clicky Game - React app.</span>
+          </div>
+        </footer> 
       </div>
     );
   }
